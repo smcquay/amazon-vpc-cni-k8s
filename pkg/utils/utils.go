61e2125f91b6c85d797fe530e0e4d53e70f8632f
Tomas Virgl <tvirgl@wish.com>
2018-02-27 10:58:23 -0800

    delete: removes unused utils/backoff
    This was previously used in the ipamd http server, but it was not
    necessary.

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -14,7 +14,6 @@
 package utils
 
 import (
-	"context"
 	"crypto/rand"
 	"encoding/binary"
 	"encoding/hex"
@@ -23,8 +22,6 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
-
-	"github.com/aws/amazon-vpc-cni-k8s/pkg/utils/ttime"
 )
 
 func DefaultIfBlank(str string, default_value string) string {
@@ -98,71 +95,6 @@ func RandHex() string {
 
 func Strptr(s string) *string {
 	return &s
-}
-
-var _time ttime.Time = &ttime.DefaultTime{}
-
-// RetryWithBackoff takes a Backoff and a function to call that returns an error
-// If the error is nil then the function will no longer be called
-// If the error is Retriable then that will be used to determine if it should be
-// retried
-func RetryWithBackoff(backoff Backoff, fn func() error) error {
-	return RetryWithBackoffCtx(context.Background(), backoff, fn)
-}
-
-// RetryWithBackoffCtx takes a context, a Backoff, and a function to call that returns an error
-// If the context is done, nil will be returned
-// If the error is nil then the function will no longer be called
-// If the error is Retriable then that will be used to determine if it should be
-// retried
-func RetryWithBackoffCtx(ctx context.Context, backoff Backoff, fn func() error) error {
-	var err error
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		default:
-		}
-
-		err = fn()
-
-		retriableErr, isRetriableErr := err.(Retriable)
-
-		if err == nil || (isRetriableErr && !retriableErr.Retry()) {
-			return err
-		}
-
-		_time.Sleep(backoff.Duration())
-	}
-	return err
-}
-
-// RetryNWithBackoff takes a Backoff, a maximum number of tries 'n', and a
-// function that returns an error. The function is called until either it does
-// not return an error or the maximum tries have been reached.
-// If the error returned is Retriable, the Retriability of it will be respected.
-// If the number of tries is exhausted, the last error will be returned.
-func RetryNWithBackoff(backoff Backoff, n int, fn func() error) error {
-	return RetryNWithBackoffCtx(context.Background(), backoff, n, fn)
-}
-
-// RetryNWithBackoffCtx takes a context, a Backoff, a maximum number of tries 'n', and a function that returns an error.
-// The function is called until it does not return an error, the context is done, or the maximum tries have been
-// reached.
-// If the error returned is Retriable, the Retriability of it will be respected.
-// If the number of tries is exhausted, the last error will be returned.
-func RetryNWithBackoffCtx(ctx context.Context, backoff Backoff, n int, fn func() error) error {
-	var err error
-	RetryWithBackoffCtx(ctx, backoff, func() error {
-		err = fn()
-		n--
-		if n == 0 {
-			// Break out after n tries
-			return nil
-		}
-		return err
-	})
-	return err
 }
 
 // Uint16SliceToStringSlice converts a slice of type uint16 to a slice of type
