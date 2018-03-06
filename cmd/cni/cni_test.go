7fff1923471f1c3aa0796cb36e60040c875adce3
Stephen McQuay (smcquay) <stephen@mcquay.me>
2018-03-06 12:20:34 -0800

    cni: squish into move

--- cmd/cni/cni_test.go
+++ cmd/cni/cni_test.go
@@ -23,10 +23,10 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/aws/amazon-vpc-cni-k8s/cmd/cni/driver/mocks"
 	"github.com/aws/amazon-vpc-cni-k8s/pkg/grpcwrapper/mocks"
 	"github.com/aws/amazon-vpc-cni-k8s/pkg/rpcwrapper/mocks"
 	"github.com/aws/amazon-vpc-cni-k8s/pkg/typeswrapper/mocks"
-	"github.com/aws/amazon-vpc-cni-k8s/plugins/routed-eni/driver/mocks"
 	"github.com/aws/amazon-vpc-cni-k8s/rpc"
 	"github.com/aws/amazon-vpc-cni-k8s/rpc/mocks"
 	"google.golang.org/grpc"
